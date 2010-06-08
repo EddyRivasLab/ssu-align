c60c993990badcbf00e29cb6298d0e50020ff0e5
Eric Nawrocki <eric.nawrocki@gmail.com>
2010-06-08 18:34:49 +0000

    o Working on cleaning up seeds/ o Changed prefix for infernal/easel/hmmer releases to 's' from 'sa'
    git-svn-id: https://svn.janelia.org/eddylab/nawrockie/src@1042 2857e7bf-25ef-0310-8806-9752169b59e1

--- trunk/seeds/build-default-models.sh
+++ trunk/seeds/build-default-models.sh
@@ -1,4 +1,4 @@
-../../src/cmbuild -F --enone --gapthresh 0.8 ssu3-0p1.cm ssu3-0p1.stk > ssu3-0p1.cmbuild
-../../src/cmbuild -F --enone --gapthresh 0.8 archaea-0p1.cm archaea-0p1.stk > archaea-0p1.cmbuild
-../../src/cmbuild -F --enone --gapthresh 0.8 bacteria-0p1.cm bacteria-0p1.stk > bacteria-0p1.cmbuild
-../../src/cmbuild -F --enone --gapthresh 0.8 eukarya-0p1.cm eukarya-0p1.stk > eukarya-0p1.cmbuild
+../infernal/src/ssu-cmbuild -F --enone --gapthresh 0.8 ssu-align-0p1.cm ssu-align-0p1.stk > ssu-align-0p1.cmbuild
+../infernal/src/ssu-cmbuild -F --enone --gapthresh 0.8 archaea-0p1.cm archaea-0p1.stk > archaea-0p1.cmbuild
+../infernal/src/ssu-cmbuild -F --enone --gapthresh 0.8 bacteria-0p1.cm bacteria-0p1.stk > bacteria-0p1.cmbuild
+../infernal/src/ssu-cmbuild -F --enone --gapthresh 0.8 eukarya-0p1.cm eukarya-0p1.stk > eukarya-0p1.cmbuild
